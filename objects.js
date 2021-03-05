ddb413b7ad72a4a0bc55e85215a8373ee1d5dff4
mmmfang <mae.fang@gmail.com>
2021-03-05 04:57:49 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,3 +2,8 @@ var playlist = {
   artist: "NCT 127",
   song: "Kick it"
 }
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  var newPlaylist = playlist, "NCT U", "Resonance";
+  return newPlaylist;
+}
